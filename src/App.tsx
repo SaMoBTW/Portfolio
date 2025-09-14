d4bf93975be4fc13acb436e2790b5a23bccdd980
Samir Mahmoud <84868862+SaMoBTW@users.noreply.github.com>
2025-09-14 17:14:16 -0400

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-
-
 function App() {
-    return (
-        <>
-            <>Home Page</>
-        </>
-    )
+  return (
+    <>
+      <body class="bg-gray-800">
+        <p class="text-white">Home Page</p>
+      </body>
+    </>
+  );
 }
 
-export default App
+export default App;
